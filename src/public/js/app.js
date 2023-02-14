8eb0188faf291a72d22d32192fc7172e267dc190
go <sukpo6010@gmail.com>
2023-02-14 11:35:19 +0900

    백앤드로 인자보내기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,11 +3,15 @@ const socket = io();
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 
+function backendDone(e) {
+  console.log(e);
+}
+
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value });
-  //오브젝트 직접 전송가능
+  socket.emit("enter_room", { payload: input.value }, backendDone);
+  //오브젝트 직접 전송가능, 함수는 인자의 마지막에 위치해야하고 함수는 프론트에서 실행.
   input.value = "";
 }
 
