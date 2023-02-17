030f8e2f7bcb3a6b42c6115159a69349daf82534
go <sukpo6010@gmail.com>
2023-02-17 18:21:28 +0900

    1234

--- src/server.js
+++ src/server.js
@@ -18,17 +18,31 @@ app.get("/", (_, res) => res.render("home"));
 // 주소가 "/" 일떄 기본디렉토리 + 인자 값을 랜더함 /src/public/views/home 을 랜더하는거지
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handlelisten = () => console.log(`Listening on http://localhost:4000`);
+const handlelisten = () => console.log(`Listening on http://localhost:4001`);
 
 const httpServer = http.createServer(app);
 
 const wsServer = new Server(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("join_room", (roomName, done) => {
+  console.log(wsServer);
+  socket.on("join_room", (roomName) => {
     socket.join(roomName);
-    done();
+    // 소켓서버의 방을 생성함.
+    socket.to(roomName).emit("welcome");
+    //1. b가 a 를 실행함.
+    socket.on("offer", (offer, roomName) => {
+      socket.to(roomName).emit("offer", offer);
+      //4. a 의 오퍼를 받고 b에게 a의 오퍼를 보냄
+    });
+    socket.on("answer", (answer, roomName) => {
+      socket.to(roomName).emit("answer", answer);
+      // 9.b의 앤서를 받고 a에게 b의 앤서를 보냄
+    });
+    socket.on("ice", (ice, roomName) => {
+      socket.to(roomName).emit("ice", ice);
+    });
   });
 });
 
-httpServer.listen(4000, handlelisten);
+httpServer.listen(4001, handlelisten);
